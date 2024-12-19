5d11db3fd8c45e85d68d8a6c35255bdb1cc7ebe6
dominiks <dominiksobanski2901@gmail.com>
2024-12-19 12:54:57 +0100

    19-12-2024

--- pythonProject/main.py
+++
@@ -1,6 +0,0 @@
-#b
-a = int(input("Wprowadz liczbe:"))
-if a > 0:
-    print("|", a,"|=", a)
-else:
-    print("|", a, "|=", -a, sep="")
--- s_31_10_24/cwiczenie2.py
+++
@@ -1,5 +0,0 @@
-
-def my_max(*args):
-    return max(args)
-
-print(my_max(1,20,2,31,3,42))
